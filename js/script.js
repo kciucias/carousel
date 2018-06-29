98ff464032b58d48c2a8750d4d65802f670c4df3
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-06-29 19:39:59 +0100

    Add js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,32 @@ var flkty = new Flickity( elem, {
   cellAlign: 'left',
   contain: true
 });
+
+// button reset
+
+var flkty = new Flickity( '.main-carousel', {
+  groupCells: true
+});
+
+var button = document.querySelectorAll('.button');
+button = fizzyUIUtils.makeArray( button );
+
+button.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var index = button.indexOf( event.target );
+  flkty.selectCell( index );
+});
+
+// progress bar
+
+var flkty = new Flickity('.main-carousel');
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
