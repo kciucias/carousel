ff13a61920b46a2d574eed9a5406c5b3f31baa97
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-07-02 19:19:21 +0100

    Add options

--- js/script.js
+++ js/script.js
@@ -1,28 +1,21 @@
-// to create carousel
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
+'use strict';
+(function(){ 
 
 // button reset
+var flkty = new Flickity('.main-carousel');
 
-var flkty = new Flickity( '.main-carousel', {
-  groupCells: true
-});
+var button = document.querySelector('.button');
 
-var button = document.querySelectorAll('.button');
-button = fizzyUIUtils.makeArray( button );
 
 button.addEventListener( 'click', function( event ) {
   // filter for button clicks
   if ( !matchesSelector( event.target, '.button' ) ) {
     return;
   }
-  var index = button.indexOf( event.target );
-  flkty.selectCell( index );
+  var index = buttons.indexOf( event.target );
+  flkty.select( index );
 });
+
 
 // progress bar
 
@@ -34,3 +27,7 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+
+
+})(); 
