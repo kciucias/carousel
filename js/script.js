4d73d104ede17cdbc005a29a6159f53c2534837c
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-07-04 18:32:08 +0100

    Change options

--- js/script.js
+++ js/script.js
@@ -1,33 +1,22 @@
+(function() { 
+
 'use strict';
-(function(){ 
 
-// button reset
-var flkty = new Flickity('.main-carousel');
-
-var button = document.querySelector('.button');
-
-
-button.addEventListener( 'click', function( event ) {
-  // filter for button clicks
-  if ( !matchesSelector( event.target, '.button' ) ) {
-    return;
-  }
-  var index = buttons.indexOf( event.target );
-  flkty.select( index );
+//carousel
+var flkty = new Flickity('.main-carousel', {
+	pageDots: false, 
+	wrapAround: true,
+	cellAlign: "left",
+	contain: true
 });
 
-
 // progress bar
-
-var flkty = new Flickity('.main-carousel');
-
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
-
 
 
 })(); 
