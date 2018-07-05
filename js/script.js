fad6731ac6058387d7c75128467262fabb04b31a
Michał Gacek <gacek.michael@outlook.com>
2018-07-05 21:18:58 +0200

    Fixes

--- js/script.js
+++ js/script.js
@@ -18,5 +18,20 @@ flkty.on( 'scroll', function( progress ) {
   progressBar.style.width = progress * 100 + '%';
 });
 
+// reset 
+document.getElementById('reset').addEventListener('click', function() {
+    flkty.select(0);
+});
+
+
+// navigation buttons
+var anchors = document.querySelectorAll('.button-group a');
+
+for (var i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener('click', function(e) {
+        var slideNumber = e.target.getAttribute('data-index');
+        flkty.select(slideNumber);
+    });
+}
 
 })(); 
